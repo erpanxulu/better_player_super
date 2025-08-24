81b9cd93206ace20ff2ec4f95f5de49ecae36c0a
erpanxulu <erpan@xulu.co.id>
2025-08-24 16:41:33 +0700

    delete exemple

--- test/mock_video_player_controller.dart
+++ test/mock_video_player_controller.dart
@@ -53,26 +53,4 @@ class MockVideoPlayerController extends VideoPlayerController {
     this.speed = speed;
   }
 
-  @override
-  Future<void> setNetworkDataSource(
-    String dataSource, {
-    VideoFormat? formatHint,
-    Map<String, String?>? headers,
-    bool useCache = false,
-    int? maxCacheSize,
-    int? maxCacheFileSize,
-    String? cacheKey,
-    bool? showNotification,
-    String? title,
-    String? author,
-    String? imageUrl,
-    String? notificationChannelName,
-    Duration? overriddenDuration,
-    String? licenseUrl,
-    String? certificateUrl,
-    Map<String, String>? drmHeaders,
-    String? activityName,
-    String? clearKey,
-    String? videoExtension,
-  }) async {}
 }
